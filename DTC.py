6db1d42facb9efc15c8304b7247cf2d1acbfef0c
Ahmed Fawzy <142937094+hix-06@users.noreply.github.com>
2024-01-11 13:24:51 +0200

    Update DTC.py

--- DTC.py
+++ DTC.py
@@ -5,7 +5,7 @@ from sklearn import metrics
 from six import StringIO  
 from IPython.display import Image  
 import pydotplus
-from sklearn.model_selection import cross_val_score
+
 
 path = r"C:\Users\Ahmed\Downloads\Iris.csv"
 data = pd.read_csv(path)
@@ -14,17 +14,16 @@ print(data.head())
 
 X = data.iloc[:, 1:-1]
 y = data.iloc[:, -1]
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 clf = DecisionTreeClassifier()
-clf = clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
-for depth in range(1, 11):
-    clf_cv = DecisionTreeClassifier(max_depth=depth)
-    scores = cross_val_score(clf_cv, X, y, cv=5)
-    print(f"Max Depth: {depth}, Cross-validated Mean Accuracy: {scores.mean()}")
+clf = clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 
 dot_data = StringIO()
@@ -33,5 +32,4 @@ export_graphviz(clf, out_file=dot_data,
                 special_characters=True, feature_names=X.columns, class_names=y.unique())
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
 Image(graph.create_png())
-
 
